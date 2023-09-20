5a6e8031c0fcbd4f96856b10dae7a6669b344331
Joonas Eloranta <joonas.eloranta@protacon.com>
2023-09-20 09:05:37 +0300

    Format support for azure function
    - Name list can now be fetched in different formats through Azure function

--- KummeliNames/__init__.py
+++ KummeliNames/__init__.py
@@ -7,6 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     countStr = req.params.get('count')
+    formatStr = req.params.get('format')
 
     print(isinstance(countStr, numbers.Real))
     if not countStr:
@@ -15,11 +16,26 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(">:(", status_code=400)
     else:
         count = int(countStr)
+
+    if not formatStr:
+        formatStr = "FirstNameLastName"
+
+    if formatStr == "FirstNameLastName":
+        format = "{0} {1}"
+    elif formatStr == "LastNameFirstName":
+        format = "{1} {0}"
+    elif formatStr == "FirstName":
+        format = "{0}"
+    elif formatStr == "LastName":
+        format = "{1}"
+    else:
+        error = ">:( Invalid format name. Supported formats are: FirstNameLastName, LastNameFirstName, FirstName, LastName"
+        return func.HttpResponse(error, status_code=400)
     
     try:
         names_list = Kummeli.getDefaultNamesList()
 
-        result = Kummeli.getNames(names_list=names_list, format="{0} {1}", count=count, allowDuplicates=None)
+        result = Kummeli.getNames(names_list=names_list, format=format, count=count, allowDuplicates=None)
         resultStr = "\n".join(str(r) for r in result)
         return func.HttpResponse(resultStr)
     except:
